2abf9dbe527f17c169c84ba8229b38f9772c2544
Louis Abramson <leabramson@gmail.com>
2023-08-21 17:19:02 -0700

    Moved the import calls to the top of gateway1.py

--- learn_python/module1/gateway1.py
+++ learn_python/module1/gateway1.py
@@ -1,11 +1,11 @@
+import sys
+from pprint import pprint
+
 print('print 1')
 
 if __name__ == '__main__':
-    import sys
-    from pprint import pprint
     print('Hello World! Python will look for code in these directories:')
     pprint(sys.path)
-    pass
 else:
     print('print 2')
 
